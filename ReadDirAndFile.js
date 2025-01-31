df0a5fd95ae41c5aa9a2aace3bbedba41c8531ce
luckytoxic <mrbalumba27@gmail.com>
2025-01-31 12:01:23 +0300

    done Controller class

--- ReadDirAndFile.js
+++ ReadDirAndFile.js
@@ -1,23 +1,28 @@
-const fsp = require('fs/promises')
-const { EOL } = require('os')
+const fsp = require('fs/promises');
+const { EOL } = require('os');
 
-class ReadFile{
-    static async readDir(){
-        return (await fsp.readdir('./topics')).map(el => el.replace(/_data.txt/,''))
-    }
+class ReadFile {
+  static async readDir() {
+    return await fsp.readdir('./topics');
+  }
 
-    static async readDirFile(num){
-        const arrayFile = await this.readDir()
-        const firstArray = await fsp.readFile(`./topics/${arrayFile[num-1]}_data.txt`,'utf-8')
-        const firstNormalArray =  firstArray.split(EOL).filter(el => el !== '') 
-        const result = []
-        for(let i = 0; i < firstNormalArray.length;i+=2){
-            result.push({question:`${firstNormalArray[i]}`,answer:`${firstNormalArray[i+1]}`})  
-        }
-        return result
+  static async readDirFile(num) {
+    try {
+      const arrayFile = await this.readDir();
+      const firstArray = await fsp.readFile(`./topics/${arrayFile[num - 1]}`, 'utf-8');
+      const firstNormalArray = firstArray.split(EOL).filter((el) => el !== '');
+      const result = [];
+      for (let i = 0; i < firstNormalArray.length; i += 2) {
+        result.push({
+          question: `${firstNormalArray[i]}`,
+          answer: `${firstNormalArray[i + 1]}`,
+        });
+      }
+      return result;
+    } catch (error) {
+      console.error('Ошибка при чтении файла:', error.message);
     }
-    
+  }
 }
 
-
-module.exports = ReadFile
+module.exports = ReadFile;
